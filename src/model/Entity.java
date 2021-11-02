487fff36d164d88d7d25899b6e91a466f283d3b3
dgoff448 <77478567+dgoff448@users.noreply.github.com>
2021-11-02 16:01:45 -0400

    Add files via upload

---
+++ src/model/Entity.java
@@ -0,0 +1,70 @@
+package model;
+
+public class Entity extends Object {
+    private int health;
+    private int x, y;
+    private int damage;
+    private int speed;
+    private int direction;
+    private int range;
+
+
+
+
+    public int getRange() {
+        return range;
+    }
+
+    public void setRange(int range) {
+        this.range = range;
+    }
+
+    public int getHealth() {
+        return health;
+    }
+
+    public void setHealth(int health) {
+        this.health = health;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public int getDamage() {
+        return damage;
+    }
+
+    public void setDamage(int damage) {
+        this.damage = damage;
+    }
+
+    public int getSpeed() {
+        return speed;
+    }
+
+    public void setSpeed(int speed) {
+        this.speed = speed;
+    }
+
+    public int getDirection() {
+        return direction;
+    }
+
+    public void setDirection(int direction) {
+        this.direction = direction;
+    }
+
+}
