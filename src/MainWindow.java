6ca6acdf67a3fa5f689972ebb751f529b295eb97
David <dgoff448@students.bju.edu>
2021-11-15 13:37:31 -0500

    Added about screen foundation

--- src/MainWindow.java
+++ src/MainWindow.java
@@ -6,6 +6,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
@@ -19,12 +20,14 @@ import javafx.scene.layout.Border;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.scene.media.AudioClip;
+import javafx.scene.text.Font;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 import javafx.stage.PopupWindow.AnchorLocation;
 
 public class MainWindow  {
     @FXML AnchorPane apaneMain;
-//TODO: FIX SIZE OF SCREEN
+
     void initialize(Stage stage) {
         AudioClip music = new AudioClip(getClass().getResource("Audio/Retro_Forest_-_David_Fesliyan.mp3").toExternalForm());
         music.play();
@@ -95,6 +98,8 @@ public class MainWindow  {
             }
         });
 
+        Aboutbtn.setOnMouseClicked(aboutEvent -> aboutScreen(aboutEvent));
+
         Startbtn.setFitHeight(40); //TODO: FIX SIZE 
         Startbtn.setFitWidth(156);
         Aboutbtn.setFitHeight(40);
@@ -106,10 +111,142 @@ public class MainWindow  {
         vbox.getChildren().add(Startbtn);
         vbox.getChildren().add(Aboutbtn);
         vbox.getChildren().add(HSbtn);
-
+        
+        vbox.setLayoutX(100);
 
         apaneMain.getChildren().add(vbox);
-    } 
+    }
+
+
+//TODO: FIX SIZE OF SCREEN
+    public void startScreen(MouseEvent e) {
+        clearVBox();
+
+        Image imgAboutScreen = new Image("Final Assets/UI/PNG/UI-StartScreen-Background-1440x900.png");
+        BackgroundImage bImg = new BackgroundImage(imgAboutScreen, 
+            BackgroundRepeat.NO_REPEAT, BackgroundRepeat.NO_REPEAT, 
+            BackgroundPosition.DEFAULT, new BackgroundSize(1.0, 1.0, true, true, false, false));
+        Background bGround = new Background(bImg);
+        apaneMain.setBackground(bGround);
+
+        VBox vbox = new VBox();
+        vbox.setAlignment(Pos.CENTER);
+        vbox.setSpacing(10.0);
+
+        Image logo = new Image("Final Assets/Logo/Final Logo.png");
+        Image start = new Image("Final Assets/UI/PNG/UI-StartBtn1-312x80.png");
+        Image about = new Image("Final Assets/UI/PNG/UI-AboutBtn1-312x80.png");
+        Image highscore = new Image("Final Assets/UI/PNG/UI-HighscoreBtn1-312x80.png");
+
+        ImageView Logo = new ImageView(logo);
+        ImageView Startbtn = new ImageView(start);
+        ImageView Aboutbtn = new ImageView(about);
+        ImageView HSbtn = new ImageView(highscore);
+
+    
+        //onMousePressed/Released-----------------------------------------------------------------------------
+        Startbtn.setOnMousePressed((EventHandler<? super MouseEvent>) new EventHandler<MouseEvent>() {
+            public void handle(MouseEvent e) {
+                Startbtn.setImage(new Image("Final Assets/UI/PNG/UI-StartBtn2-312x80.png"));
+            }
+        });
+        Startbtn.setOnMouseReleased((EventHandler<? super MouseEvent>) new EventHandler<MouseEvent>() {
+            public void handle(MouseEvent e) {
+                Startbtn.setImage(new Image("Final Assets/UI/PNG/UI-StartBtn1-312x80.png"));
+            }
+        });
+
+        Aboutbtn.setOnMousePressed((EventHandler<? super MouseEvent>) new EventHandler<MouseEvent>() {
+            public void handle(MouseEvent e) {
+                Aboutbtn.setImage(new Image("Final Assets/UI/PNG/UI-AboutBtn2-312x80.png"));
+            }
+        });
+        Aboutbtn.setOnMouseReleased((EventHandler<? super MouseEvent>) new EventHandler<MouseEvent>() {
+            public void handle(MouseEvent e) {
+                Aboutbtn.setImage(new Image("Final Assets/UI/PNG/UI-AboutBtn1-312x80.png"));
+            }
+        });
+
+        HSbtn.setOnMousePressed((EventHandler<? super MouseEvent>) new EventHandler<MouseEvent>() {
+            public void handle(MouseEvent e) {
+                HSbtn.setImage(new Image("Final Assets/UI/PNG/UI-HighscoreBtn2-312x80.png"));
+            }
+        });
+        HSbtn.setOnMouseReleased((EventHandler<? super MouseEvent>) new EventHandler<MouseEvent>() {
+            public void handle(MouseEvent e) {
+                HSbtn.setImage(new Image("Final Assets/UI/PNG/UI-HighscoreBtn1-312x80.png"));
+            }
+        });
+        //-----------------------------------------------------------------------------------------------
+
+
+        Startbtn.setOnMouseClicked(event -> {
+            try {
+                onStartClicked(event);
+            } catch (IOException e1) {
+                e1.printStackTrace();
+            }
+        });
+
+        Aboutbtn.setOnMouseClicked(aboutEvent -> aboutScreen(aboutEvent));
+
+        Startbtn.setFitHeight(40); //TODO: FIX SIZE 
+        Startbtn.setFitWidth(156);
+        Aboutbtn.setFitHeight(40);
+        Aboutbtn.setFitWidth(156);
+        HSbtn.setFitHeight(40);
+        HSbtn.setFitWidth(156);
+
+        vbox.getChildren().add(Logo);
+        vbox.getChildren().add(Startbtn);
+        vbox.getChildren().add(Aboutbtn);
+        vbox.getChildren().add(HSbtn);
+        
+        vbox.setLayoutX(100);
+
+        apaneMain.getChildren().add(vbox);
+    }
+
+    //Mouse Pressed/Released------------------------------------------------------------------------------
+    public void aboutScreen(MouseEvent e) {
+        clearVBox();
+
+        Image back = new Image("Final Assets/UI/PNG/UI-BackBtn1-312x80.png");
+        ImageView backBtn = new ImageView(back);
+
+        backBtn.setOnMousePressed((EventHandler<? super MouseEvent>) new EventHandler<MouseEvent>() {
+            public void handle(MouseEvent e) {
+                backBtn.setImage(new Image("Final Assets/UI/PNG/UI-BackBtn2-312x80.png"));
+            }
+        });
+        backBtn.setOnMouseReleased((EventHandler<? super MouseEvent>) new EventHandler<MouseEvent>() {
+            public void handle(MouseEvent e) {
+                backBtn.setImage(new Image("Final Assets/UI/PNG/UI-BackBtn1-312x80.png"));
+            }
+        });
+        //-------------------------------------------------------------------------------------------------
+
+        backBtn.setFitHeight(40);
+        backBtn.setFitWidth(156);
+        backBtn.setOnMouseClicked(backEvent -> startScreen(backEvent));
+
+        VBox vbox = new VBox();
+        vbox.setAlignment(Pos.CENTER);
+        vbox.setSpacing(10.0);
+        vbox.getChildren().add(backBtn);
+        vbox.setLayoutX(270);
+
+        apaneMain.getChildren().add(vbox);
+    }
+
+    public void clearVBox() {
+        try {
+            apaneMain.getChildren().remove(0);
+        } catch (NullPointerException n) {
+
+        }
+
+    }
 
     public void onStartClicked(MouseEvent e) throws IOException {
         var loader = new FXMLLoader(getClass().getResource("GameWindow.fxml"));
@@ -124,3 +261,16 @@ public class MainWindow  {
     }
 
 }
+
+
+
+
+
+
+//Custom font code?-----------------------------------------------------
+// Font font = Font.loadFont("Final Assets/UI/Pixel Font.ttf", 45.0);
+// Label lbl = new Label();
+// lbl.setText("Hello");
+// lbl.setFont(font);
+// vbox.getChildren().add(lbl);
+//----------------------------------------------------------------------
