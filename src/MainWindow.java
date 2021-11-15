b76e0d7f1f2a7ac3f3dac0f5a3637b3b0cdb5a7b
David <dgoff448@students.bju.edu>
2021-11-15 16:11:02 -0500

    condensed code in MainWindow.java

--- src/MainWindow.java
+++ src/MainWindow.java
@@ -32,94 +32,12 @@ public class MainWindow  {
         AudioClip music = new AudioClip(getClass().getResource("Audio/Retro_Forest_-_David_Fesliyan.mp3").toExternalForm());
         music.play();
 
-        Image imgAboutScreen = new Image("Final Assets/UI/PNG/UI-StartScreen-Background-1440x900.png");
-        BackgroundImage bImg = new BackgroundImage(imgAboutScreen, 
-            BackgroundRepeat.NO_REPEAT, BackgroundRepeat.NO_REPEAT, 
-            BackgroundPosition.DEFAULT, new BackgroundSize(1.0, 1.0, true, true, false, false));
-        Background bGround = new Background(bImg);
-        apaneMain.setBackground(bGround);
-
-        VBox vbox = new VBox();
-        vbox.setAlignment(Pos.CENTER);
-        vbox.setSpacing(10.0);
-
-        Image logo = new Image("Final Assets/Logo/Final Logo.png");
-        Image start = new Image("Final Assets/UI/PNG/UI-StartBtn1-312x80.png");
-        Image about = new Image("Final Assets/UI/PNG/UI-AboutBtn1-312x80.png");
-        Image highscore = new Image("Final Assets/UI/PNG/UI-HighscoreBtn1-312x80.png");
-
-        ImageView Logo = new ImageView(logo);
-        ImageView Startbtn = new ImageView(start);
-        ImageView Aboutbtn = new ImageView(about);
-        ImageView HSbtn = new ImageView(highscore);
-
-    
-        //onMousePressed/Released-----------------------------------------------------------------------------
-        Startbtn.setOnMousePressed((EventHandler<? super MouseEvent>) new EventHandler<MouseEvent>() {
-            public void handle(MouseEvent e) {
-                Startbtn.setImage(new Image("Final Assets/UI/PNG/UI-StartBtn2-312x80.png"));
-            }
-        });
-        Startbtn.setOnMouseReleased((EventHandler<? super MouseEvent>) new EventHandler<MouseEvent>() {
-            public void handle(MouseEvent e) {
-                Startbtn.setImage(new Image("Final Assets/UI/PNG/UI-StartBtn1-312x80.png"));
-            }
-        });
-
-        Aboutbtn.setOnMousePressed((EventHandler<? super MouseEvent>) new EventHandler<MouseEvent>() {
-            public void handle(MouseEvent e) {
-                Aboutbtn.setImage(new Image("Final Assets/UI/PNG/UI-AboutBtn2-312x80.png"));
-            }
-        });
-        Aboutbtn.setOnMouseReleased((EventHandler<? super MouseEvent>) new EventHandler<MouseEvent>() {
-            public void handle(MouseEvent e) {
-                Aboutbtn.setImage(new Image("Final Assets/UI/PNG/UI-AboutBtn1-312x80.png"));
-            }
-        });
-
-        HSbtn.setOnMousePressed((EventHandler<? super MouseEvent>) new EventHandler<MouseEvent>() {
-            public void handle(MouseEvent e) {
-                HSbtn.setImage(new Image("Final Assets/UI/PNG/UI-HighscoreBtn2-312x80.png"));
-            }
-        });
-        HSbtn.setOnMouseReleased((EventHandler<? super MouseEvent>) new EventHandler<MouseEvent>() {
-            public void handle(MouseEvent e) {
-                HSbtn.setImage(new Image("Final Assets/UI/PNG/UI-HighscoreBtn1-312x80.png"));
-            }
-        });
-        //-----------------------------------------------------------------------------------------------
-
-
-        Startbtn.setOnMouseClicked(e -> {
-            try {
-                onStartClicked(e);
-            } catch (IOException e1) {
-                e1.printStackTrace();
-            }
-        });
-
-        Aboutbtn.setOnMouseClicked(aboutEvent -> aboutScreen(aboutEvent));
-
-        Startbtn.setFitHeight(40); //TODO: FIX SIZE 
-        Startbtn.setFitWidth(156);
-        Aboutbtn.setFitHeight(40);
-        Aboutbtn.setFitWidth(156);
-        HSbtn.setFitHeight(40);
-        HSbtn.setFitWidth(156);
-
-        vbox.getChildren().add(Logo);
-        vbox.getChildren().add(Startbtn);
-        vbox.getChildren().add(Aboutbtn);
-        vbox.getChildren().add(HSbtn);
-        
-        vbox.setLayoutX(100);
-
-        apaneMain.getChildren().add(vbox);
+        startScreen();
     }
 
 
 //TODO: FIX SIZE OF SCREEN
-    public void startScreen(MouseEvent e) {
+    public void startScreen() {
         clearVBox();
 
         Image imgAboutScreen = new Image("Final Assets/UI/PNG/UI-StartScreen-Background-1440x900.png");
@@ -228,7 +146,7 @@ public class MainWindow  {
 
         backBtn.setFitHeight(40);
         backBtn.setFitWidth(156);
-        backBtn.setOnMouseClicked(backEvent -> startScreen(backEvent));
+        backBtn.setOnMouseClicked(backEvent -> callStartScreen(backEvent));
 
         VBox vbox = new VBox();
         vbox.setAlignment(Pos.CENTER);
@@ -239,10 +157,14 @@ public class MainWindow  {
         apaneMain.getChildren().add(vbox);
     }
 
+    public void callStartScreen(MouseEvent e) {
+        startScreen();
+    }
+
     public void clearVBox() {
         try {
             apaneMain.getChildren().remove(0);
-        } catch (NullPointerException n) {
+        } catch (IndexOutOfBoundsException e) {
 
         }
 
